1686c9cf54312e399c710a2db03cea52eaedf757
deepzeedi <44259143+deepzeedi@users.noreply.github.com>
2019-05-23 15:49:44 +0300

    install

---
+++ install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+echo "----install Virtual environment----"
+pip3 install virtualenv
+
+cd ~/
+echo ''
+echo '---Загружаю репозиторий Blog---'
+echo ''
+git clone https://github.com/deepzeedi/Blog.git
+
+
+cd ~/Blog
+echo ''
+echo '---Устанавливаю виртуальное окружение venv---'
+echo ''
+
+virtualenv venv
+
+
+
+echo ''
+if test -f './venv/bin/activate'
+then 
+    echo '---Активирую виртуальное окружение---'
+    source venv/bin/activate
+else
+    echo 'xxx---виртуальное окружение не установлено!'
+fi
+
+
+echo ''
+echo '---Устанавливаю Flask---'
+echo ''
+
+pip install flask
+
+echo ''
+echo '---Устанавливаю Flask Admin---'
+echo ''
+
+pip install flask_admin
+
+echo ''
+echo '---Устанавливаю Flask QLAlchemy---'
+echo ''
+
+pip install Flask-SQLAlchemy
+
+echo ''
+echo '---Устанавливаю Flask Migrate---'
+echo ''
+
+pip install Flask-Migrate
+
+echo ''
+echo '---Устанавливаю Flask Script---'
+echo ''
+
+pip install flask_script
+
+echo ''
+echo '---Устанавливаю Flask Security---'
+echo ''
+
+pip install flask_security
+
+echo ''
+echo '---Устанавливаю Flask jsonify---'
+echo ''
+
+pip install jsonify
+
+echo ''
+echo '---Устанавливаю requests---'
+echo ''
+
+pip install requests
+
+
+echo '---Открываю браузер---'
+echo '--------Удачи!--------'
+x-www-browser http://localhost:5000
+
+echo ''
+echo '---Запускаю сайт---'
+echo '---на  localhost---'
+echo ''
+
+cd app
+python main.py
+
+
+
